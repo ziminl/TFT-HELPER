bc3b1bb284edd46f2f24e53fdad03ec060d20382
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-07-19 08:21:41 +0900

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -22,4 +22,27 @@
 #
 
 ###########code############
-#deck
+#pip install pillow pytesseract pygetwindow pyautogui
+
+from PIL import Image
+import pytesseract
+import pyautogui
+
+# Tesseract setup
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+left = 200
+top = 400
+width = 200
+height = 400
+box = (left, top, left + width, top + height)
+
+screenshot = pyautogui.screenshot(region=box)
+
+screenshot.save('captured_region.png')
+
+screenshot = screenshot.convert('L')  # Convert to grayscale
+
+text = pytesseract.image_to_string(screenshot)
+
+print("200~400x200x400 has :", text)
